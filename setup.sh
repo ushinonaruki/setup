a1e88ab2a6156274a210e38fbd498c9a4164ee12
ushinonaruki <ushinonaruki@gmail.com>
2021-12-28 21:08:11 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,36 +12,49 @@ email="${name}@gmail.com"
 # variable: docker-compose version
 dcv="1.26.0"
 
+# echo: firewall
+echo '----- ufw -----'
+
 # ファイアウォールの起動
 echo $pass | sudo ufw enable
 
 # packageの最新化
 echo $pass | sudo apt update
 
-# gitのインストール
-echo $pass | sudo apt-get install git
+# echo: git
+echo '----- git -----'
 
-# gitの実行確認
-dpkg -l git
+# gitのインストール
+echo $pass | yes | sudo apt-get install git
 
 # gitの初期設定
 git config --global user.name $name
 git config --global user.email $email
 
-# dockerのインストール
-echo $pass | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-echo $pass | sudo apt update
-echo $pass | sudo apt install docker-ce
+# echo: docker
+echo '----- docker -----'
 
-# dockerの実行確認
-echo $pass | sudo systemctl status docker
+# aptがHTTPS経由でパッケージを使用できるようにするいくつかの必要条件パッケージをインストール
+echo $pass | yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+# 公式DockerリポジトリのGPGキーをシステムに追加
+echo $pass | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# DockerリポジトリをAPTソースに追加
+echo $pass | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+# 追加されたリポジトリからDockerパッケージでパッケージデータベースを更新
+echo $pass | sudo apt update
+
+# Dockerをインストール
+echo $pass | yes | sudo apt install docker-ce
 
 # SudoなしでDockerコマンドを実行できるよう設定
 echo $pass | sudo usermod -aG docker ${USER}
-su - ${USER}
+echo $pass | su - ${USER}
 
-# ユーザーがdockerグループに追加されたことを確認
-id -nG
+# echo: docker
+echo '----- docker-compose -----'
 
 # docker-composeをインストール
 echo $pass | sudo curl -L "https://github.com/docker/compose/releases/download/${dcv}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -49,11 +62,29 @@ echo $pass | sudo curl -L "https://github.com/docker/compose/releases/download/$
 # docker-composeコマンドが実行可能になるように権限を設定
 echo $pass | sudo chmod +x /usr/local/bin/docker-compose
 
-# docker-composeの実行確認
-docker-compose --version
+# echo: lan driver
+echo '----- lan driver -----'
 
 # ネットワークドライバのインストール Realtek_r8125
 cd lan_driver
 docker-compose up
-cd ..
 
+
+# echo: 実行確認
+echo '----- 実行確認 -----'
+
+# gitの実行確認
+echo '----- git version -----'
+git version
+
+# ユーザーがdockerグループに追加されたことを確認
+echo '----- id -nG -----'
+id -nG
+
+# dockerの実行確認
+echo '----- sudo systemctl status docker -----'
+docker version
+
+# docker-composeの実行確認
+echo '----- docker-compose --version -----'
+docker-compose --version
